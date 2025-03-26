7d4b9796e9be3da75003fd3009323f07b2f6bf32
aaphar <apharmagaramova@gmail.com>
2025-03-26 10:59:06 +0400

    search insert position

--- src/binarySearch/SearchInsertPosition.java
+++ src/binarySearch/SearchInsertPosition.java
@@ -4,6 +4,8 @@ package binarySearch;
  * https://leetcode.com/problems/search-insert-position/description/
  */
 public class SearchInsertPosition {
+    // Time complexity O(n log n)
+    // Space complexity O(1)
     public int searchInsert(int[] nums, int target) {
         int start = 0;
         int end = nums.length - 1;
@@ -18,5 +20,16 @@ public class SearchInsertPosition {
             }
         }
         return start;
+    }
+
+    // Time complexity O(n)
+    // Space complexity O(1)
+    public int searchInsertForLoop(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= target) {
+                return i;
+            }
+        }
+        return nums.length;
     }
 }
