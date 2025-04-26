5817872ada1cedc00c9c52267a65106e958e67be
aaphar <apharmagaramova@gmail.com>
2025-04-26 20:55:39 +0400

    FindTheMostCommonResponse

---
+++ src/string/FindTheMostCommonResponse.java
@@ -0,0 +1,53 @@
+package string;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * https://leetcode.com/problems/find-the-most-common-response/
+ */
+public class FindTheMostCommonResponse {
+    public static void main(String[] args) {
+        List<List<String>> responses = List.of(
+                List.of("good", "ok", "good"),
+                List.of("ok", "bad"),
+                List.of("bad", "notsure"),
+                List.of("great", "good")
+        );
+
+//        good -2, ok -2, bad -2, notsure -1, great -1
+
+        FindTheMostCommonResponse findTheMostCommonResponse = new FindTheMostCommonResponse();
+        String result = findTheMostCommonResponse.findCommonResponse(responses);
+        System.out.println(result); // Output: "good"
+    }
+
+    public String findCommonResponse(List<List<String>> responses) {
+        Map<String, Integer> map = new HashMap<>();
+        int max = Integer.MIN_VALUE;
+
+        for (List<String> response : responses) {
+            Set<String> set = Set.copyOf(response);
+            for (String word : set) {
+                int count = map.getOrDefault(word, 0) + 1;
+                if (count > max) {
+                    max = count;
+                }
+                map.put(word, count);
+            }
+        }
+
+        String result = null;
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == max) {
+                if (result == null || entry.getKey().compareTo(result) < 0) {
+                    result = entry.getKey();
+                }
+            }
+        }
+
+        return result;
+    }
+}
