86be8ae175e8ba8019bf635d81d834b0ee0bf0d9
aaphar <apharmagaramova@gmail.com>
2025-05-15 02:12:18 +0400

    Boyer-Moore Voting Algorithm, majorityElement

--- src/MajorityElement.java
+++ src/MajorityElement.java
@@ -7,11 +7,28 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class MajorityElement {
     public static void main(String[] args) {
-        int[] nums = {3, 2, 3};
+        int[] nums = {1, 1, 2, 2, 1, 2, 2};
         System.out.println(new MajorityElement().majorityElement(nums));
     }
 
+    //  Boyer-Moore Voting Algorithm.
+    // Time complexity O(n)
+    // Space complexity O(1)
     public int majorityElement(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+
+    public int majorityElementW(int[] nums) {
         AtomicInteger result = new AtomicInteger();
         int majority = nums.length / 2;
         Map<Integer, Integer> elementCount = new HashMap<>();
