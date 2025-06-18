00654e117eac54bb66b68e1018806bc2873d73fd
aaphar <apharmagaramova@gmail.com>
2025-06-18 13:40:08 +0400

    pascal triangle revisit

--- src/dynamicProgramming/PascalTriangle.java
+++ src/dynamicProgramming/PascalTriangle.java
@@ -18,49 +18,24 @@ public class PascalTriangle {
         }
     }
 
-    // TODO
-    public List<List<Integer>> generateBetter(int n) {
-        List<List<Integer>> res = new ArrayList<>();
-        for (int i = 1; i <= n; i++) {
-            List<Integer> temp = new ArrayList<>();
-            int ans = 1;
-            temp.add(ans);
-            for (int j = 1; j < i; j++) {
-                ans = ans * (i - j);
-                ans = ans / j;
-                temp.add(ans);
-            }
-            res.add(temp);
-        }
-        return res;
-
-    }
-
     public static List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> triangle = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
         if (numRows == 0) {
-            return triangle;
+            return result;
         }
-
-        triangle.add(new ArrayList<>());
-        triangle.getFirst().add(1);
-
+        result.add(new ArrayList<>(List.of(1)));
 
         for (int i = 1; i < numRows; i++) {
-            List<Integer> prev_row = triangle.get(i - 1);
-            List<Integer> new_row = new ArrayList<>();
-
-            new_row.add(1);
-            for (int j = 1; j < prev_row.size(); j++) {
-                new_row.add(prev_row.get(j - 1) + prev_row.get(j));
+            List<Integer> row = new ArrayList<>();
+            List<Integer> prevRow = result.get(i - 1);
+            row.add(1);
+            for (int j = 1; j < i; j++) {
+                row.add(prevRow.get(j - 1) + prevRow.get(j));
             }
-            new_row.add(1);
-            triangle.add(new_row);
-
+            row.add(1);
+            result.add(row);
         }
-
-
-        return triangle;
+        return result;
     }
 }
 
@@ -70,6 +45,4 @@ public class PascalTriangle {
  * Row 3:      [1, 2, 1]
  * Row 4:     [1, 3, 3, 1]
  * Row 5:    [1, 4, 6, 4, 1]
- *
- *
  */
